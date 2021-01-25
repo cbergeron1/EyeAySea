b6a67fba430303471390e96ed9a7840fc6831044
Carlton Bergeron <dio@Carltons-MBP.lan>
2021-01-25 00:36:18 -0500

    Fix local

--- Mothership/vars.tf
+++ Mothership/vars.tf
@@ -7,7 +7,7 @@ locals {
   tfe_token         = var.TFE_Token
   oauth_token       = var.oAuth_Token_ID
   github_org        = var.github_org
-  github_repo       = var.github_repo
+  github_repo       = var.repo_name
   github_identifier = format("%s/%s", local.github_org, local.github_repo)
 }
 
