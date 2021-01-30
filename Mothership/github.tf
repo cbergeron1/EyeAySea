4803500772f6af9c9b7b14d9bc13b9385d76b8c1
Carlton Bergeron <dio@Carltons-MBP.lan>
2021-01-30 00:37:42 -0500

    Delete repository

--- Mothership/github.tf
+++ Mothership/github.tf
@@ -1,12 +1,3 @@
 ###############################
 # Managed Github Repositories #
 ###############################
-
-resource "github_repository" "Ticker" {
-  name        = "Ticker"
-  description = "Python Discord bot that pulls stock information about the provided ticker."
-
-  visibility         = "private"
-  has_issues         = true
-  gitignore_template = "Python"
-}
