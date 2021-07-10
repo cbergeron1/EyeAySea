9e0324adb18295c0511b110b9b3c015d8b933025
Carlton Bergeron <dio@Carltons-MBP.lan>
2021-07-10 12:22:00 -0400

    Remove precommit

--- Mothership/tfe.tf
+++ Mothership/tfe.tf
@@ -16,7 +16,7 @@ resource "tfe_workspace" "infrastructure" {
   organization = tfe_organization.primary.id
 
   auto_apply        = true
-  terraform_version = "1.0.2"
+  terraform_version = "1.0.1"
   working_directory = "Mothership"
 
   vcs_repo {
